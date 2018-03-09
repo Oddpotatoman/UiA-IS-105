fa4cc0675edf59744d70e480e43441e629b1375a
Odd Andre Vatne <oddi160@gmail.com>
2018-03-09 18:05:03 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Oblig2/Oppgave 1/fileinfo.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+)
+
+
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+	fi, err := os.Lstat(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mode := fi.Mode()
+	fmt.Println("Filename")
+	fmt.Println(fi.Name())
+	if mode.IsRegular() {
+		fmt.Println("regular file")
+	}else {
+		fmt.Println("Is not regular file")
+	}
+	if mode.IsDir() {
+		fmt.Println("directory")
+	}else {
+		fmt.Println("Is not directory")
+	}
+	if mode&os.ModeSymlink != 0 {
+		fmt.Println("symbolic link")
+	}else {
+		fmt.Println("Is not Symbbolic link")
+	}
+	if mode&os.ModeNamedPipe != 0{
+		fmt.Println("named pipe")
+	}else {
+		fmt.Println("Is not named pipe")
+	}
+	fmt.Println(mode.Perm())
+	fmt.Println(fi.Size())
+
+}
