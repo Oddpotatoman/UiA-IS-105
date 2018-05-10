e0a9e54ceffe2320aeac92b19793c6809caf84ed
Odd Andre Vatne <oddi160@gmail.com>
2018-05-10 14:33:20 +0200

    Add complete functionality

--- Oblig4/webserver.go
+++ Oblig4/webserver.go
@@ -1,19 +1,17 @@
 package main
 
-
 import (
 	"html/template"
 	"net/http"
-	"time"
 	"io/ioutil"
 	"log"
 	"path"
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"strconv"
 	"os"
 )
+
 type Temps struct {
 	XMLName xml.Name `xml:"temperature"`
 	Value   string   `xml:"value,attr"`
@@ -24,116 +22,70 @@ type Ws struct {
 	Mps  string `xml:"mps,attr"`
 	Name string `xml:"name,attr"`
 }
+type Symbol struct {
+	XMLName xml.Name `xml:"symbol"`
+	Name string `xml:"name,attr"`
+}
 type Times struct {
 	XMLName     xml.Name `xml:"time"`
 	From        string   `xml:"from,attr"`
 	To          string   `xml:"to,attr"`
 	Period      string   `xml:"period,attr"`
-	Temperature Temps  `xml:"temperature"`
-	WindSpeed   Ws     `xml:"windSpeed"`
+	Temperature Temps    `xml:"temperature"`
+	WindSpeed   Ws       `xml:"windSpeed"`
+	Symbol      Symbol   `xml:"symbol"`
 }
-type Time struct {
-	XMLName xml.Name `xml:"time"`
-	From        string   `xml:"from,attr"`
-	To          string   `xml:"to,attr"`
-	EventType string `xml:"eventType,attr"`
-}
-type Location struct {
-	XMLName xml.Name `xml:"location"`
-	Time Time `xml:"time"`
-}
-type Text struct {
-	XMLName xml.Name `xml:"text"`
-	Location Location `xml:"location"`
-}
-
 type Forecasts struct {
 	XMLName xml.Name `xml:"forecast"`
 	Tabular []Times  `xml:"tabular>time"`
-	Text Text `xml:"text"`
 }
 type weatherdata struct {
 	XMLName  xml.Name  `xml:"weatherdata"`
 	Forecast Forecasts `xml:"forecast"`
 }
 
-func main () {
-	http.HandleFunc("/1", api1)
-	http.HandleFunc("/5", weatherSuggestion)
+func main() {
+	//Make a quick index
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fp := path.Join("Oblig4/templates/index.html")
+
+		tmpl, err := template.ParseFiles(fp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if err := tmpl.Execute(w, "index"); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+
+	http.HandleFunc("/sandnes", func(w http.ResponseWriter, r *http.Request) {
+		weatherSuggestion(w, r, "Rogaland/Sandnes/Sandnes")
+	})
+	http.HandleFunc("/oslo", func(w http.ResponseWriter, r *http.Request) {
+		weatherSuggestion(w, r, "Oslo/Oslo/Oslo")
+	})
+	http.HandleFunc("/kristiansand", func(w http.ResponseWriter, r *http.Request) {
+		weatherSuggestion(w, r, "Vest-Agder/Kristiansand/Kristiansand")
+	})
 	http.ListenAndServe(":8080", nil)
 }
-func api1 (w http.ResponseWriter, r * http.Request){
 
-	url := "https://hotell.difi.no/api/json/stavanger/miljostasjoner"
-	apiClient := http.Client{
-		Timeout: time.Second * 2, // Maximum of 2 secs
-	}
-	type Entry struct {
-		Latitude float32 `json:"latitude,string"`
-		Navn string `json:"navn"`
-		Plast string `json:"plast"`
-		Glass_metall string `json:"glass_metall"`
-		Tekstil_sko string `json:"tekstil_sko"`
-		Longitude float32 `json:"longitude,string"`
-	}
-	type Entries struct {
-		Entries []Entry `json:"entries"`
-		Page int `json:"page"`
-		Pages int `json:"pages"`
-		Posts int `json:"posts"`
+func weatherSuggestion(w http.ResponseWriter, r *http.Request, extension string) {
 
-	}
-
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res, getErr := apiClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	var entires Entries
-	jsonErr := json.Unmarshal(body, &entires)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-	//Very varied when this works or not
-	fp := path.Join("templates","api1.html")
-
-	tmpl, err := template.ParseFiles(fp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := tmpl.Execute(w, entires); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func weatherSuggestion(w http.ResponseWriter, r * http.Request){
-
-	extension := "Rogaland/Sandnes/Sandnes"
-
-	url := "http://www.yr.no/sted/Norge/"+extension+"/varsel.xml"
+	url := "http://www.yr.no/sted/Norge/" + extension + "/varsel.xml"
 
 	var XMLweather = ""
-	if cryptoCurrency, err := getXML(url); err != nil {
+	if getWdFromUrl, err := getXML(url); err != nil {
 		log.Printf("Failed to get XML: %v", err)
 	} else {
 		log.Println("Received XML:")
-		log.Println(cryptoCurrency)
-		XMLweather = cryptoCurrency
+		log.Println(getWdFromUrl)
+		XMLweather = getWdFromUrl
 	}
 
-	fp := path.Join("Oblig4/templates/api5.html")
+	fp := path.Join("Oblig4/templates/suggestion.html")
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -144,33 +96,72 @@ func weatherSuggestion(w http.ResponseWriter, r * http.Request){
 	if xmlErr != nil {
 		log.Fatal(xmlErr)
 	}
-	var message string
-	var weatherType = weatherData.Forecast.Text.Location.Time.EventType
+	var weatherType = weatherData.Forecast.Tabular[0].Symbol.Name
 	var totalTemp = convertStringToInt(weatherData.Forecast.Tabular[0].Temperature.Value)
 	var totalWs = convertStringToFloat(weatherData.Forecast.Tabular[0].WindSpeed.Mps)
-
-	if weatherType == "Rain" {
-		message = "Det regner idag, bruk regnjakke."
-		if totalTemp > 15 {
-			message += " Det er også ganske varmt, så ha ett tynt lag med klær under."
-		} else if totalTemp > 10 {
-			message += " Det er en grei varme ute, så ikke bombarder deg med klær."
-		} else if totalTemp > 0 {
-			message += " Det er også ganske kjølig ute, vi foreslår at du kler deg godt."
-			} else {
-				message += " Det er også SYKT kaldt ute så vi foreslår du kler deg utroligt godt #ullErGull"
-		}
-	}
 
 	println(weatherType)
 	println(totalWs)
 	println(totalTemp)
-	if err := tmpl.Execute(w, XMLweather); err != nil {
+	if err := tmpl.Execute(w, getMessage(totalTemp, totalWs, weatherType)); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 }
-func convertStringToFloat(toFloat string)(float64){
+func getMessage(temp int, ws float64, wt string) (string) {
+	//Disse værtypene var de jeg fant ifra yr. Siden vi ikke har all verdens tid så gjør vi det ikke altfor detaljert.
+	var message string
+	if wt == "Regn" || wt == "Kraftig regn" || wt == "Kraftige regnbyger" {
+		message = "Det regner idag, bruk regnjakke."
+		if temp > 15 {
+			message += " Det er også ganske varmt, så ha ett tynt lag med klær under."
+		} else if temp > 10 {
+			message += " Det er en grei varme ute, så ikke bombarder deg med klær."
+		} else if temp > 0 {
+			message += " Det er også ganske kjølig ute, vi foreslår at du kler deg godt."
+		} else {
+			message += " Det er også SYKT kaldt ute så vi foreslår du kler deg utroligt godt #ullErGull"
+		}
+	} else if wt == "Skyet" || wt == "Lettskyet" || wt == "Delvis skyet" {
+		message = "Det er skyet ute."
+		if temp > 20 {
+			message += " Det er også varmt da, så T-skjorte og bukse høres greit ut."
+		} else if temp > 10 {
+			message += " Det er en grei varme ute, så ikke bombarder deg med klær men ikke gå lettkledd heller."
+		} else if temp > 0 {
+			message += " Det er også ganske kjølig ute, vi foreslår at du kler deg godt."
+		} else {
+			message += " Det er også SYKT kaldt ute så vi foreslår du kler deg utroligt godt #ullErGull"
+		}
+	} else if wt == "Klarvær" || wt == "Sol" {
+		message = "Det er sol ute, husk solkrem."
+		if temp > 20 {
+			message += " Det er også varmt da, så hiv på en t-skjorte og shorts."
+		} else if temp > 10 {
+			message += " Det er en grei varme ute, så en t-skjorte, genser og bukse vil funke fett."
+		} else if temp > 0 {
+			message += " Det er fortsatt ganske kjølig ute da så hiv på en jakke i tilegg til genser og bukse."
+		} else {
+			message += " Det er også SYKT kaldt ute så vi foreslår du kler deg utroligt godt #ullErGull"
+		}
+	} else {
+		message = ""
+		if temp > 20 {
+			message += "Det er varmt ute, hiv på deg sommeklær og hopp i vannet."
+		} else if temp > 10 {
+			message += "Det er litt smålig kjølig ute, hiv på deg en jakke."
+		} else if temp > 0 {
+			message += "Her var det litt kaldt altså, litt tykkere klær en vanelig kan være greit."
+		} else {
+			message += " Det er SYKT kaldt ute så vi foreslår du kler deg utroligt godt #ullErGull"
+		}
+		if ws > 10 {
+			message += " Det blåser en del så vær litt obs på det."
+		}
+	}
+	return message
+}
+func convertStringToFloat(toFloat string) (float64) {
 	f, err := strconv.ParseFloat(toFloat, 64)
 	if err == nil {
 		/** displayg the type of the b variable */
@@ -181,7 +172,7 @@ func convertStringToFloat(toFloat string)(float64){
 	}
 	return f
 }
-func convertStringToInt(toInt string)(int){
+func convertStringToInt(toInt string) (int) {
 	convertedInt, err := strconv.Atoi(toInt)
 	if err != nil {
 		// handle error
